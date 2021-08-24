ebc5e656bbf5e19c8147012b18e3b57597557901
jbytecode <mhsatman@gmail.com>
2021-08-24 23:33:52 +0300

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject diffjl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject com.github.jbytecode/diffjl "0.1.0"
+  :description "Derivative of single variable functions and expression simplifier"
+  :url "https://github.com/jbytecode/diff.clj"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot diffclj.core
   :target-path "target/%s"
