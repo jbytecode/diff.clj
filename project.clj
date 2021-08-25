9d89702268b9b722806dd16fbe90205ac2dfeb6a
jbytecode <mhsatman@gmail.com>
2021-08-25 13:46:22 +0300

    v0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.jbytecode/diffclj "0.1.0"
+(defproject com.github.jbytecode/diffclj "0.1.1"
   :description "Derivative of single variable functions and expression simplifier"
   :url "https://github.com/jbytecode/diff.clj"
   :license {:name "MIT"
